c630bb96efe343b8b1c704b8fcb1647fdb40f22a
Test <hab@keine.config>
2017-07-15 09:36:03 +0000

    salt und pw angepasst

--- simplesamlphp/config/config.php
+++ simplesamlphp/config/config.php
@@ -75,7 +75,7 @@ $config = array(
      * metadata listing and diagnostics pages.
      * You can also put a hash here; run "bin/pwgen.php" to generate one.
      */
-    'auth.adminpassword' => '123',
+    'auth.adminpassword' => 'phpadfstest',
     'admin.protectindexpage' => false,
     'admin.protectmetadata' => false,
 
@@ -87,7 +87,7 @@ $config = array(
      * A possible way to generate a random salt is by running the following command from a unix shell:
      * tr -c -d '0123456789abcdefghijklmnopqrstuvwxyz' </dev/urandom | dd bs=32 count=1 2>/dev/null;echo
      */
-    'secretsalt' => 'defaultsecretsalt',
+    'secretsalt' => 'phpadfstest',
 
     /*
      * Some information about the technical persons running this installation.
